144143b84226563e187ceb7e35532d00df296f95
kabiru <ibrahimkabiru29@gmail.com>
2022-07-25 17:03:20 -0400

    Creating file for insurace
    Signed-off-by: kabiru <ibrahimkabiru29@gmail.com>

---
+++ Models/Insurance.cs
@@ -0,0 +1,27 @@
+namespace Models
+{
+    public class Insurance
+    {
+        public string provider { get; set; }
+        public string benefactor { get; set; }
+
+        public insurance()
+        {
+             provider = null;
+             benefactor= null;
+             
+        }
+        // Used for creating a insurance
+        public Insurance(string provider, string benefactor)
+        {
+            this.provider = provider;
+            this.benefactor = benefactor;
+        }
+        // Used for getting all insurance
+        public Insurance(string provider, string benefactor)
+        {
+            this.provider = provider;
+            this.benefactor = be;
+        }
+    }
+}
