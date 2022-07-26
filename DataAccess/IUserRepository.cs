e83859d7a99269fd08effb4e9aee45fd630caf7e
krisholl <krisholl2022@gmail.com>
2022-07-26 10:30:17 -0400

    Merging

---
+++ DataAccess/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DataAccess
+{
+    public interface IUserRepository
+    {
+        public List<Users> GetAllUsers();
+        public Users GetUserById(int userID);
+        public Users GetUserByName(string userName);
+        public Users GetUserByEmail(string email);
+        public Users CreateUser(Users user);
+        public void ResetPassword(Users user);
+    }
+}
