318e008f759b0996b396f538758428082fe3df36
krisholl <krisholl2022@gmail.com>
2022-07-26 17:32:43 -0400

    Began DBContext creation
    Signed-off-by: krisholl <krisholl2022@gmail.com>

--- DataAccess/UserRepository.cs
+++ DataAccess/UserRepository.cs
@@ -5,31 +5,33 @@ using Microsoft.Data.SqlClient;
 namespace DataAccess;
 public class UserRepository : IUserRepository
 {
-    private readonly ConnectionFactory _connectionFactory;
-
-    public UserRepository(ConnectionFactory factory)
+    public User CreateUser(User user)
     {
-        _connectionFactory = factory;
+        throw new NotImplementedException();
     }
-    public List<Users> GetAllUsers()
-    {
-        List<Users> users = new List<Users>();
-        SqlConnection conn = _connectionFactory.GetConnection();
-        conn.Open();
-        SqlCommand cmd = new SqlCommand("select * from p2.users", conn);
-        SqlDataReader reader = cmd.ExecuteReader();
 
-        while(reader.Read())
-        {
-            users.Add(new Users((string)reader[1], (char)reader[2], (String)reader[3], (string)reader[4], (string)reader[5], (DateOnly)reader[6], (int)reader[7], (int)reader[8]));
-        }
-        reader.Close();
-        conn.Close();
-        return users;
+    public List<User> GetAllUsers()
+    {
+        throw new NotImplementedException();
     }
-    public Users GetUserById(int userID);
-    public Users GetUserByName(string userName);
-    public Users GetUserByEmail(string email);
-    public Users CreateUser(Users user);
-    public void ResetPassword(Users user);
+
+    public User GetUserByEmail(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public User GetUserById(int userID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public User GetUserByName(string userName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ResetPassword(User user)
+    {
+        throw new NotImplementedException();
+    }
 }
---
+++ Models/Claim.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public enum Status
+    {
+        Pending,Approved,Denied
+    }
+    public class Claim
+    {
+        [Key]
+        public int claimID { get; set; }
+        public int userID { get; set; }
+        public int doctorID { get; set; }
+        public int policyID {get; set; }
+        public FileStream? reasonForVisit { get; set; }
+        public Status status { get; set; }
+        public Claim()
+        {
+            claimID = 0;
+            userID = 0;
+            doctorID = 0;
+            policyID = 0;
+            reasonForVisit = null;
+            status = Status.Pending;
+        }
+        //For Making a claim
+        public Claim( int userIDFK, int doctorIDFK, int providerIDFK, FileStream reasonForVisit ):this()
+        { 
+            this.userID = userIDFK;
+            this.doctorID= doctorIDFK;
+            this.policyID = providerIDFK;
+            this.reasonForVisit = reasonForVisit; 
+        }
+        public Claim(int claimID, int userIDFK, int doctorIDFK, int providerIDFK, FileStream? reasonForVisit, Status status)
+        {
+            this.claimID = claimID;
+            this.userID = userIDFK;
+            this.doctorID = doctorIDFK;
+            this.policyID = providerIDFK;
+            this.reasonForVisit = reasonForVisit;
+            this.status = status;
+        }
+    }
+}
