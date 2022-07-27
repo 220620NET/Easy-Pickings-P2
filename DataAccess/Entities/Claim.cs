270eaa67811e7786fb2671f78243d7d3ea20bbc2
krisholl <krisholl2022@gmail.com>
2022-07-27 10:35:58 -0400

    Entities from scaffolding
    Signed-off-by: krisholl <krisholl2022@gmail.com>

---
+++ DataAccess/Entities/Claim.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Entities
+{
+    public partial class Claim
+    {
+        public int ClaimId { get; set; }
+        public int? UserIdFk { get; set; }
+        public int? DoctorIdFk { get; set; }
+        public int? ProviderFk { get; set; }
+        public byte[] ReasonForVisit { get; set; } = null!;
+        public string Status { get; set; } = null!;
+
+        public virtual User? DoctorIdFkNavigation { get; set; }
+        public virtual Policy? ProviderFkNavigation { get; set; }
+        public virtual User? UserIdFkNavigation { get; set; }
+    }
+}
---
+++ DataAccess/Entities/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Entities
+{
+    public partial class User
+    {
+        public User()
+        {
+            ClaimDoctorIdFkNavigations = new HashSet<Claim>();
+            ClaimUserIdFkNavigations = new HashSet<Claim>();
+            Policies = new HashSet<Policy>();
+            Providers = new HashSet<Policy>();
+        }
+
+        public int UserId { get; set; }
+        public string? FirstName { get; set; }
+        public string? MiddleInit { get; set; }
+        public string? LastName { get; set; }
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public DateTime? DoB { get; set; }
+        public int? ContactFk { get; set; }
+        public string Role { get; set; } = null!;
+
+        public virtual Contact? ContactFkNavigation { get; set; }
+        public virtual ICollection<Claim> ClaimDoctorIdFkNavigations { get; set; }
+        public virtual ICollection<Claim> ClaimUserIdFkNavigations { get; set; }
+        public virtual ICollection<Policy> Policies { get; set; }
+
+        public virtual ICollection<Policy> Providers { get; set; }
+    }
+}
