717f7181837101691b27c83ba43e09b0d6536dfc
kabiru <ibrahimkabiru29@gmail.com>
2022-07-28 16:26:43 -0400

    Updating the policy id

--- DataAccess/IPolicy.cs
+++ DataAccess/IPolicy.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Entities; 
 
 namespace DataAccess
 {
     public interface IPolicy
     {
-       public List<Policy> GetPolicyByPolicyID(int policyID);
+       public List<Policy> GetPolicyByID(int policyID);
        public List<Policy> GetAllPolicy();
-       public List<Policy> GetPolicyByuserID(int insurance);
-      public List<Policy> GetPolicyBycoverage(FileStream coverage);
+       public List<Policy> GetPolicyByInsurance(int insurance);
+      public List<Policy> GetPolicyBycoverage(byte[] coverage);
     }
 }
