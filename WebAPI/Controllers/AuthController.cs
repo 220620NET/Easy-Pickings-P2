30548e1d3c5924ea1d6bf22f64e06b26cccd3f1d
krisholl <krisholl2022@gmail.com>
2022-07-29 11:09:43 -0400

    AuthController class and Endpoints
    Signed-off-by: krisholl <krisholl2022@gmail.com>

--- WebAPI/Controllers/AuthController.cs
+++ WebAPI/Controllers/AuthController.cs
@@ -2,3 +2,34 @@ using Services;
 using DataAccess.Entities;
 using DataAccess;
 namespace WebAPI.Controllers;
+
+public class AuthController
+{
+    private readonly AuthService _authService;
+    public AuthController(AuthService authService)
+    {
+        _authService = authService;
+    }
+    public IResult Login(string? username, string? password)
+    {
+        try
+        {
+            return Results.Accepted("/login",_authService.Login(username, password));
+        }
+        catch (NotImplementedException)
+        {
+            return Results.BadRequest();
+        }
+    }
+    public IResult Register(User userToRegister)
+    {
+        try
+        {
+            return Results.Accepted("/register",_authService.Register(userToRegister));
+        }
+        catch (NotImplementedException)
+        {
+            return Results.BadRequest();
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -21,19 +21,43 @@ builder.Services.AddScoped<IUserRepo, UserRepo>();
 */
 builder.Services.AddTransient<AuthService>();
 builder.Services.AddTransient<TicketService>();
-builder.Services.AddTransient<UserService>();
-builder.Services.AddTransient<ClaimsService>();
-builder.Services.AddTransient<ContactService>();
+//builder.Services.AddTransient<UserService>();
+//builder.Services.AddTransient<ClaimsService>();
+//builder.Services.AddTransient<ContactService>();
 builder.Services.AddTransient<PolicyService>();
 /*
  *      Controler Layer Scoping
 */
 builder.Services.AddScoped<AuthController>();
-builder.Services.AddScoped<TicketController>();
-builder.Services.AddScoped<UserController>();
-builder.Services.AddScoped<PolicyController>();
-builder.Services.AddScoped<ClaimsController>();
-builder.Services.AddScoped<ContactController>();
+//builder.Services.AddScoped<TicketController>();
+//builder.Services.AddScoped<UserController>();
+//builder.Services.AddScoped<PolicyController>();
+//builder.Services.AddScoped<ClaimsController>();
+//builder.Services.AddScoped<ContactController>();
 app.MapGet("/", () => "Hello World!");
+/*
+ *      AuthController End Points
+*/
+app.MapPost("/login",(string? username, string? password, AuthController controller)=>controller.Login(username, password));
+app.MapPost("/register", (User newUser, AuthController controller) => controller.Register(newUser));
+/*
+ *      UserController End Points
+*/
+
+/*
+ *      TicketController End Points
+ */
+
+/*
+ *      PolicyController End Points
+ */
+
+/*
+ *      ClaimsController End Points
+ */
+
+/*
+ *      ContactController End Points
+ */
 
 app.Run();
