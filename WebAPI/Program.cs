77e7a8d31cc379bfd6c0092bcc42ee66893188eb
krisholl <krisholl2022@gmail.com>
2022-07-29 08:17:46 -0400

    Removing DBContext not in use
    Signed-off-by: krisholl <krisholl2022@gmail.com>

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -4,7 +4,7 @@ using DataAccess;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-builder.Services.AddDbContext<InsuranceDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("P2DB")));
+builder.Services.AddDbContext<easypickingsContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("P2DB")));
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
