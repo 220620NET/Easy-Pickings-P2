4e7b022d63e48462485d3e25fa8a15975afd7047
krisholl <krisholl2022@gmail.com>
2022-08-03 17:58:57 -0400

    Small project
    Signed-off-by: krisholl <krisholl2022@gmail.com>

---
+++ MiniProj/index.js
@@ -0,0 +1,16 @@
+
+function findTrail(a,b){
+    console.log('HI');
+    let elem = document.getElementById('post-code');
+    let zipInput= elem.value;
+    if(zipInput){
+        fetch('http://api.zippopotam.us/us/'+zipInput).then((response)=>response.json()).then((resBody)=>{
+            places = resBody['places'][0];
+            document.querySelector('#result-zipcode').innerText = resBody['post code']; 
+            document.querySelector('#result-city').innerText = places['place name'];
+            document.querySelector('#result-state').innerText=places['state abbreviation'];
+            document.querySelector('#result-num').innerText = a;
+            document.querySelector('#result-name').innerText=b;
+        })
+    }
+}
