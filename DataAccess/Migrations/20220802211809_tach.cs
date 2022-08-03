cdbd26e53b8f2422eef18c7f2a4fc28fa28db908
krisholl <krisholl2022@gmail.com>
2022-08-03 10:30:55 -0400

    Migrations
    Signed-off-by: krisholl <krisholl2022@gmail.com>

---
+++ DataAccess/Migrations/20220802211809_tach.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class tach : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "middleInit",
+                table: "users",
+                newName: "middle_init");
+
+            migrationBuilder.RenameColumn(
+                name: "lastName",
+                table: "users",
+                newName: "last_name");
+
+            migrationBuilder.RenameColumn(
+                name: "firstName",
+                table: "users",
+                newName: "first_name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "middle_init",
+                table: "users",
+                newName: "middleInit");
+
+            migrationBuilder.RenameColumn(
+                name: "last_name",
+                table: "users",
+                newName: "lastName");
+
+            migrationBuilder.RenameColumn(
+                name: "first_name",
+                table: "users",
+                newName: "firstName");
+        }
+    }
+}
---
+++ DataAccess/Migrations/20220803134102_schemas.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class schemas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.RenameTable(
+                name: "users",
+                newName: "users",
+                newSchema: "dbo");
+
+            migrationBuilder.RenameTable(
+                name: "tickets",
+                newName: "tickets",
+                newSchema: "dbo");
+
+            migrationBuilder.RenameTable(
+                name: "policies",
+                newName: "policies",
+                newSchema: "dbo");
+
+            migrationBuilder.RenameTable(
+                name: "contact",
+                newName: "contact",
+                newSchema: "dbo");
+
+            migrationBuilder.RenameTable(
+                name: "claims",
+                newName: "claims",
+                newSchema: "dbo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "users",
+                schema: "dbo",
+                newName: "users");
+
+            migrationBuilder.RenameTable(
+                name: "tickets",
+                schema: "dbo",
+                newName: "tickets");
+
+            migrationBuilder.RenameTable(
+                name: "policies",
+                schema: "dbo",
+                newName: "policies");
+
+            migrationBuilder.RenameTable(
+                name: "contact",
+                schema: "dbo",
+                newName: "contact");
+
+            migrationBuilder.RenameTable(
+                name: "claims",
+                schema: "dbo",
+                newName: "claims");
+        }
+    }
+}
