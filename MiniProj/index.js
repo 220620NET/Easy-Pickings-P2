42eda3e13e5b16d49d2fd167d10ab0b96dc456cb
krisholl <krisholl2022@gmail.com>
2022-08-05 10:47:35 -0400

    Metallica
    Signed-off-by: krisholl <krisholl2022@gmail.com>

--- MiniProj/index.js
+++ MiniProj/index.js
@@ -1,15 +1,23 @@
-var y =new Audio("the_star_spangled_banner_64kb.mp3");
+var y =new Audio();
 const setTheme = theme =>{
     document.documentElement.className = theme;
     if(theme ==='fourth'){
+        y.src = "murica.mp3";
         y.play(); 
-    }else{
+    }
+    else if(theme === 'hokie'){
+        y.src ="hokie_theme.mp3";
+        y.play();
+    } else if(theme ==='spirit'){
+        
+    }
+    else{
         y.pause();
         y.currentTime=0;
     }
 };
 
-function findTrail(a,b){
+function findTrail(a,b,c){
     console.log('HI');
     let elem = document.getElementById('post-code');
     let zipInput= elem.value;
@@ -20,7 +28,12 @@ function findTrail(a,b){
             document.querySelector('#result-city').innerText = places['place name'];
             document.querySelector('#result-state').innerText=places['state abbreviation'];
             document.querySelector('#result-num').innerText = a;
-            document.querySelector('#result-name').innerText=b; 
+            document.querySelector('#result-name').innerText = b; 
+            document.querySelector('result-user').innerText=c;
         })
     }
+}
+function mute(){
+    y.pause();
+    y.currentTime=0;
 }
