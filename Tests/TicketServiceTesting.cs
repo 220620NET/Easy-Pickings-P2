70415b1b4e1d4545b3f9294f011fcd0b320969c1
krisholl <krisholl2022@gmail.com>
2022-08-09 16:22:05 -0400

    Finished TicketService Testing
    Signed-off-by: krisholl <krisholl2022@gmail.com>

--- Tests/TicketServiceTesting.cs
+++ Tests/TicketServiceTesting.cs
@@ -10,6 +10,9 @@ namespace Tests
 {
     public class TicketServiceTesting
     {
+        /*
+         *      CreateTicket() Testing
+         */
         [Fact]
         public void TicketCreationFailsWithInvalidClaim()
         {
@@ -190,6 +193,318 @@ namespace Tests
             mockedTicket.Setup(r => r.GetAllTickets()).Returns(ticket1);
             TicketService ticketService = new(mockedTicket.Object, mockedPolicy.Object, mockedUser.Object, mockedClaim.Object);
             Assert.Throws<InvalidUserException>(() => ticketService.CreateTicket(ticket));
+        }
+
+        /*      
+         *      UpdateTicket() Testing
+         */
+        [Fact]
+        public void TicketUpdateFailsWithNonExistentTicket()
+        {
+
+            var mockedTicket = new Mock<ITicket>();
+            var mockedUser = new Mock<IUserRepo>();
+            var mockedPolicy = new Mock<IPolicy>();
+            var mockedClaim = new Mock<IClaimRepo>();
+            List<User> user = new();
+            user.Add(new()
+            {
+                userID = 2,
+                first_name = "Joanne",
+                middle_init = 'T',
+                last_name = "Scammer",
+                username = "Micke",
+                password = "1234",
+                DoB = new DateTime(),
+                role = "Employee"
+            });
+            List<Claim> claim = new();
+            claim.Add(new()
+            {
+                claimID = 1,
+                userID = 2,
+                doctorID = 1,
+                policyID = 1,
+                reasonForVisit = "Test"
+            });
+            List<Policy> policy = new List<Policy>();
+            policy.Add(new()
+            {
+                policyID = 1,
+                insurance = 3,
+                coverage = "Test"
+            });
+
+            List<Ticket> ticket1 = new();
+            ticket1.Add(new()
+            {
+                ticketID = 1,
+                policyID = 1,
+                claimID = 1,
+                userID = 2,
+                details = "Test"
+            });
+            Ticket ticket = new()
+            {
+                ticketID = 2,
+                policyID = 1,
+                claimID = 1,
+                userID = 2,
+                details = "Test"
+            };
+            mockedUser.Setup(r => r.GetAllUsers()).Returns(user);
+            mockedClaim.Setup(r => r.GetAllClaims()).Returns(claim);
+            mockedPolicy.Setup(r => r.GetAllPolicy()).Returns(policy);
+            mockedTicket.Setup(r => r.GetAllTickets()).Returns(ticket1);
+            TicketService ticketService = new(mockedTicket.Object, mockedPolicy.Object, mockedUser.Object, mockedClaim.Object);
+            Assert.Throws<TicketNotAvailable>(() => ticketService.UpdateTicket(ticket));
+        }
+        [Fact]
+        public void TicketUpdateFailsWithInvalidClaim()
+        {
+
+            var mockedTicket = new Mock<ITicket>();
+            var mockedUser = new Mock<IUserRepo>();
+            var mockedPolicy = new Mock<IPolicy>();
+            var mockedClaim = new Mock<IClaimRepo>();
+            List<User> user = new();
+            user.Add(new()
+            {
+                userID = 2,
+                first_name = "Joanne",
+                middle_init = 'T',
+                last_name = "Scammer",
+                username = "Micke",
+                password = "1234",
+                DoB = new DateTime(),
+                role = "Employee"
+            });
+            List<Claim> claim = new();
+            claim.Add(new()
+            {
+                claimID = 1,
+                userID = 2,
+                doctorID = 1,
+                policyID = 1,
+                reasonForVisit = "Test"
+            });
+            List<Policy> policy = new List<Policy>();
+            policy.Add(new()
+            {
+                policyID = 1,
+                insurance = 3,
+                coverage = "Test"
+            });
+
+            List<Ticket> ticket1 = new();
+            ticket1.Add(new()
+            {
+                ticketID = 1,
+                policyID = 1,
+                claimID = 1,
+                userID = 2,
+                details = "Test"
+            });
+            Ticket ticket = new()
+            {
+                ticketID = 1,
+                policyID = 1,
+                claimID = 2,
+                userID = 2,
+                details = "Test"
+            };
+            mockedUser.Setup(r => r.GetAllUsers()).Returns(user);
+            mockedClaim.Setup(r => r.GetAllClaims()).Returns(claim);
+            mockedPolicy.Setup(r => r.GetAllPolicy()).Returns(policy);
+            mockedTicket.Setup(r => r.GetAllTickets()).Returns(ticket1);
+            TicketService ticketService = new(mockedTicket.Object, mockedPolicy.Object, mockedUser.Object, mockedClaim.Object);
+            Assert.Throws<InvalidClaimException>(() => ticketService.UpdateTicket(ticket));
+        }
+        [Fact]
+        public void TicketUpdateFailsWithInvalidPolicy()
+        {
+            var mockedTicket = new Mock<ITicket>();
+            var mockedUser = new Mock<IUserRepo>();
+            var mockedPolicy = new Mock<IPolicy>();
+            var mockedClaim = new Mock<IClaimRepo>();
+            List<User> user = new();
+            user.Add(new()
+            {
+                userID = 2,
+                first_name = "Joanne",
+                middle_init = 'T',
+                last_name = "Scammer",
+                username = "Micke",
+                password = "1234",
+                DoB = new DateTime(),
+                role = "Employee"
+            });
+            List<Claim> claim = new();
+            claim.Add(new()
+            {
+                claimID = 1,
+                userID = 2,
+                doctorID = 1,
+                policyID = 1,
+                reasonForVisit = "Test"
+            });
+            List<Policy> policy = new List<Policy>();
+            policy.Add(new()
+            {
+                policyID = 1,
+                insurance = 3,
+                coverage = "Test"
+            });
+
+            List<Ticket> ticket1 = new();
+            ticket1.Add(new()
+            {
+                ticketID = 1,
+                policyID = 1,
+                claimID = 1,
+                userID = 2,
+                details = "Test"
+            });
+            Ticket ticket = new()
+            {
+                ticketID = 1,
+                policyID = 2,
+                claimID = 1,
+                userID = 2,
+                details = "Test"
+            };
+
+            mockedUser.Setup(r => r.GetAllUsers()).Returns(user);
+            mockedClaim.Setup(r => r.GetAllClaims()).Returns(claim);
+            mockedPolicy.Setup(r => r.GetAllPolicy()).Returns(policy);
+            mockedTicket.Setup(r => r.GetAllTickets()).Returns(ticket1);
+            TicketService ticketService = new(mockedTicket.Object, mockedPolicy.Object, mockedUser.Object, mockedClaim.Object);
+            Assert.Throws<InvalidPolicyException>(() => ticketService.UpdateTicket(ticket));
+        }
+        [Fact]
+        public void TicketUpdateFailsWithInvalidUser()
+        {
+
+            var mockedTicket = new Mock<ITicket>();
+            var mockedUser = new Mock<IUserRepo>();
+            var mockedPolicy = new Mock<IPolicy>();
+            var mockedClaim = new Mock<IClaimRepo>();
+            List<User> user = new();
+            user.Add(new()
+            {
+                userID = 2,
+                first_name = "Joanne",
+                middle_init = 'T',
+                last_name = "Scammer",
+                username = "Micke",
+                password = "1234",
+                DoB = new DateTime(),
+                role = "Employee"
+            });
+            List<Claim> claim = new();
+            claim.Add(new()
+            {
+                claimID = 1,
+                userID = 2,
+                doctorID = 1,
+                policyID = 1,
+                reasonForVisit = "Test"
+            });
+            List<Policy> policy = new List<Policy>();
+            policy.Add(new()
+            {
+                policyID = 1,
+                insurance = 3,
+                coverage = "Test"
+            });
+
+            List<Ticket> ticket1 = new();
+            ticket1.Add(new()
+            {
+                ticketID = 1,
+                policyID = 1,
+                claimID = 1,
+                userID = 2,
+                details = "Test"
+            });
+            Ticket ticket = new()
+            {
+                ticketID = 1,
+                policyID = 1,
+                claimID = 1,
+                userID = 1,
+                details = "Test"
+            };
+            mockedUser.Setup(r => r.GetAllUsers()).Returns(user);
+            mockedClaim.Setup(r => r.GetAllClaims()).Returns(claim);
+            mockedPolicy.Setup(r => r.GetAllPolicy()).Returns(policy);
+            mockedTicket.Setup(r => r.GetAllTickets()).Returns(ticket1);
+            TicketService ticketService = new(mockedTicket.Object, mockedPolicy.Object, mockedUser.Object, mockedClaim.Object);
+            Assert.Throws<InvalidUserException>(() => ticketService.UpdateTicket(ticket));
+        }
+
+        /*
+         *      DeleteTicket() Testing
+         */
+        [Fact]
+        public void TicketDeleteFailsWithNonExistentTicket()
+        {
+            var mockedTicket = new Mock<ITicket>();
+            var mockedUser = new Mock<IUserRepo>();
+            var mockedPolicy = new Mock<IPolicy>();
+            var mockedClaim = new Mock<IClaimRepo>();
+            List<User> user = new();
+            user.Add(new()
+            {
+                userID = 2,
+                first_name = "Joanne",
+                middle_init = 'T',
+                last_name = "Scammer",
+                username = "Micke",
+                password = "1234",
+                DoB = new DateTime(),
+                role = "Employee"
+            });
+            List<Claim> claim = new();
+            claim.Add(new()
+            {
+                claimID = 1,
+                userID = 2,
+                doctorID = 1,
+                policyID = 1,
+                reasonForVisit = "Test"
+            });
+            List<Policy> policy = new List<Policy>();
+            policy.Add(new()
+            {
+                policyID = 1,
+                insurance = 3,
+                coverage = "Test"
+            });
+
+            List<Ticket> ticket1 = new();
+            ticket1.Add(new()
+            {
+                ticketID = 1,
+                policyID = 1,
+                claimID = 1,
+                userID = 2,
+                details = "Test"
+            });
+            Ticket ticket = new()
+            {
+                ticketID = 2,
+                policyID = 1,
+                claimID = 1,
+                userID = 2,
+                details = "Test"
+            };
+            mockedUser.Setup(r => r.GetAllUsers()).Returns(user);
+            mockedClaim.Setup(r => r.GetAllClaims()).Returns(claim);
+            mockedPolicy.Setup(r => r.GetAllPolicy()).Returns(policy);
+            mockedTicket.Setup(r => r.GetAllTickets()).Returns(ticket1);
+            TicketService ticketService = new(mockedTicket.Object, mockedPolicy.Object, mockedUser.Object, mockedClaim.Object);
+            Assert.Throws<TicketNotAvailable>(() => ticketService.DeleteTicket(ticket.ticketID));
         }
     }
 }
