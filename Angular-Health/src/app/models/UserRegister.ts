3626bc4cc5f89e53cfdaaff85da0a66495575522
krisholl <krisholl2022@gmail.com>
2022-08-12 15:40:14 -0400

    Login Done
    Signed-off-by: krisholl <krisholl2022@gmail.com>

---
+++ Angular-Health/src/app/models/UserRegister.ts
@@ -0,0 +1,10 @@
+export interface UserRegister{
+    
+    firstName: string;
+    middleInitial: string;
+    lastName: string;
+    username: string;
+    password: string;
+    DoB: string;
+    role: string;
+}
