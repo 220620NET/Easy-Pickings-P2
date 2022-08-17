23834426a6b1c8d8ed0c797c394194f6f50bd0f2
sonia-bhattacharya15 <sonia.s.bhattacharya@gmail.com>
2022-08-17 09:05:23 -0700

    Added contact table

--- Angular-Health/src/app/app.module.ts
+++ Angular-Health/src/app/app.module.ts
@@ -1,26 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser'; 
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { StartScreenComponent } from './start-screen/start-screen.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ClaimTableComponent } from './claim-table/claim-table.component';
 import { PolicyTableComponent } from './policy-table/policy-table.component';
 import { TicketTableComponent } from './ticket-table/ticket-table.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { AngularWebStorageModule } from 'angular-web-storage';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
- 
+import { ContactTableComponent } from './contact-table/contact-table.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,18 +19,12 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     ProfileComponent,
     ClaimTableComponent,
     PolicyTableComponent,
-    TicketTableComponent, 
+    TicketTableComponent,
+    ContactTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, 
-    ReactiveFormsModule,
-    HttpClientModule,
-    NoopAnimationsModule,
-    MatToolbarModule,
-    AngularWebStorageModule,
-    MatInputModule,
-    MatSidenavModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
